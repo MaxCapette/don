669f698f4b53f3ba764803b966e349528308876b
Dokuyaku <sk8baz@gmail.com>
2024-06-21 20:20:09 +0200

    add search bar

--- src/datas/PokemonList.tsx
+++ src/datas/PokemonList.tsx
@@ -1,19 +1,12 @@
-// app/PokemonList.tsx
 "use client";
-// app/PokemonList.tsx
 import Link from "next/link";
 import React, { useState, useEffect } from "react";
-import { capitalizeFirstLetter } from "../lib/utils"; // Importez la fonction utilitaire
+import { capitalizeFirstLetter } from "../lib/utils";
 import Image from "next/image";
 
-// Constant pour le nombre de Pokémon à charger à chaque fois
-const ITEMS_PER_PAGE = 100;
-
 // Fonction pour récupérer les Pokémon depuis l'API
-async function fetchPokemons(offset: number = 0) {
-  const res = await fetch(
-    `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${ITEMS_PER_PAGE}`
-  );
+async function fetchAllPokemons() {
+  const res = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=1000`);
   if (!res.ok) {
     throw new Error("Failed to fetch data");
   }
@@ -21,42 +14,62 @@ async function fetchPokemons(offset: number = 0) {
 }
 
 export default function PokemonList() {
-  const [pokemons, setPokemons] = useState<any[]>([]);
-  const [nextOffset, setNextOffset] = useState(0);
+  const [allPokemons, setAllPokemons] = useState<any[]>([]);
+  const [displayedPokemons, setDisplayedPokemons] = useState<any[]>([]);
+  const [searchTerm, setSearchTerm] = useState("");
 
-  // Fonction pour charger plus de Pokémon
-  const loadMorePokemons = async () => {
-    try {
-      const data = await fetchPokemons(nextOffset);
-      const newPokemons = await Promise.all(
-        data.results.map(async (result: any) => {
-          const res = await fetch(result.url);
-          if (!res.ok) {
-            throw new Error("Failed to fetch Pokemon details");
-          }
-          return res.json();
-        })
-      );
-      setPokemons((prevPokemons) => [...prevPokemons, ...newPokemons]);
-      setNextOffset(nextOffset + ITEMS_PER_PAGE);
-    } catch (error) {
-      console.error("Error loading more pokemons:", error);
-    }
-  };
-
-  // Au premier rendu, charger les premiers Pokémon
+  // Charger tous les Pokémon une fois
   useEffect(() => {
-    loadMorePokemons();
+    const loadAllPokemons = async () => {
+      try {
+        const data = await fetchAllPokemons();
+        const detailedPokemons = await Promise.all(
+          data.results.map(async (result: any) => {
+            const res = await fetch(result.url);
+            if (!res.ok) {
+              throw new Error("Failed to fetch Pokemon details");
+            }
+            return res.json();
+          })
+        );
+        setAllPokemons(detailedPokemons);
+        setDisplayedPokemons(detailedPokemons);
+      } catch (error) {
+        console.error("Error loading all pokemons:", error);
+      }
+    };
+
+    loadAllPokemons();
   }, []);
+
+  // Fonction de gestion de la recherche
+  const handleSearchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const value = event.target.value.toLowerCase();
+    setSearchTerm(value);
+
+    const filteredPokemons = allPokemons.filter((pokemon) =>
+      pokemon.name.toLowerCase().includes(value)
+    );
+    setDisplayedPokemons(filteredPokemons);
+  };
 
   return (
     <div className="container mx-auto">
-      <h1 className=" font-bold text-center my-8 px-6 py-4 text-3xl">Pokemon List</h1>
-      <div className="flex flex-wrap justify-center gap  ">
-        {pokemons.map((pokemon: any) => (
+      <h1 className="font-bold text-center my-8 px-6 py-4 text-3xl">Pokemon List</h1>
+      <div className="flex justify-center mb-8">
+        <input
+          type="text"
+          placeholder="Search Pokémon"
+          className="px-4 py-2 border rounded w-1/2"
+          value={searchTerm}
+          onChange={handleSearchChange}
+        />
+      </div>
+      <div className="flex flex-wrap justify-center gap-4">
+        {displayedPokemons.map((pokemon: any) => (
           <div
             key={pokemon.name}
-            className="max-w-sm overflow-hidden shadow-lg  hover:scale-110 transition duration-300 glassmorphism-container"
+            className="max-w-sm overflow-hidden shadow-lg hover:scale-110 transition duration-300 glassmorphism-container"
           >
             <Link href={`/pokedex/${pokemon.name}`}>
               {pokemon.sprites && (
@@ -75,14 +88,6 @@ export default function PokemonList() {
             </Link>
           </div>
         ))}
-      </div>
-      <div className="flex justify-center my-8">
-        <button
-          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-          onClick={loadMorePokemons}
-        >
-          Load More
-        </button>
       </div>
     </div>
   );
