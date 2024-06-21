9dffa300f6505f42b702abf70b347d8bfb33c91c
Dokuyaku <sk8baz@gmail.com>
2024-06-21 19:57:49 +0200

    add some pokemon details

--- src/app/pokedex/[name]/page.tsx
+++ src/app/pokedex/[name]/page.tsx
@@ -1,31 +1,153 @@
 // app/[name].tsx
-import Image from 'next/image';
-import React from 'react';
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
 
 // Fetching data from PokeAPI
 async function fetchPokemon(name: string) {
   const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`);
   if (!res.ok) {
-    throw new Error('Failed to fetch data');
+    throw new Error("Failed to fetch data");
   }
   return res.json();
 }
 
-export default async function PokemonDetail({ params }: { params: { name: string } }) {
+export default async function PokemonDetail({
+  params,
+}: {
+  params: { name: string };
+}) {
   const pokemon = await fetchPokemon(params.name);
 
   return (
-    <div>
-      <h1>{pokemon.name}</h1>
-      <p>Height: {pokemon.height}</p>
-      <p>Weight: {pokemon.weight}</p>
-      <Image src={pokemon.sprites.front_default}
-       alt={pokemon.name}
-       width={200}
-       height={200}
-        />
-      
-      <p>Type: {pokemon.types.map((type: any) => type.type.name).join(', ')}</p>
+    <div className="container mx-auto p-4">
+      <div className="flex justify-between">
+        <h1 className="text-4xl font-bold capitalize">{pokemon.name}</h1>
+        <Link href="/pokedex">
+          <button className="bg-gray-500 hover:bg-gray-700 text-white font-bold py-1 px-3 rounded m-1">
+            Back
+          </button>
+        </Link>
+      </div>
+    <div className="flex flex-col items-center" >
+      <Image
+        src={pokemon.sprites.other["official-artwork"].front_default}
+        alt={pokemon.name}
+        width={400}
+        height={400}
+        className="mx-auto"
+      />
+      <div className="mt-4">
+        <p className="text-lg">
+          <strong>Height:</strong> {pokemon.height} decimetres
+        </p>
+        <p className="text-lg">
+          <strong>Weight:</strong> {pokemon.weight} hectograms
+        </p>
+        <p className="text-lg">
+          <strong>Type:</strong>{" "}
+          {pokemon.types.map((type: any) => type.type.name).join(", ")}
+        </p>
+        <p className="text-lg">
+          <strong>Abilities:</strong>{" "}
+          {pokemon.abilities
+            .map((ability: any) => ability.ability.name)
+            .join(", ")}
+        </p>
+        <div className="flex gap-10">
+        <div className="mt-4">
+          <h2 className="text-2xl font-bold">Base Stats</h2>
+          <ul className="list-disc list-inside">
+            {pokemon.stats.map((stat: any) => (
+              <li key={stat.stat.name} className="text-sm">
+                <strong>{stat.stat.name}:</strong> {stat.base_stat}
+              </li>
+            ))}
+          </ul>
+        </div>
+        <div className="mt-4">
+          <h2 className="text-2xl font-bold">Moves</h2>
+          <ul className="list-disc list-inside">
+            {pokemon.moves.slice(0, 10).map(
+              (
+                move: any // Limiter le nombre de mouvements affichés
+              ) => (
+                <li key={move.move.name} className="text-sm">
+                  {move.move.name}
+                </li>
+              )
+            )}
+          </ul>
+        </div>
+        </div>
+          {/* Afficher les cris */}
+          <div className="mt-4 flex flex-col gap-4">
+          <h2 className="text-2xl font-bold">Cries</h2>
+          <audio controls>
+            <source src={`https://raw.githubusercontent.com/PokeAPI/cries/main/cries/pokemon/latest/${pokemon.id}.ogg`} type="audio/ogg" />
+            Your browser does not support the audio element.
+          </audio>
+          <audio controls >
+            <source src={`https://raw.githubusercontent.com/PokeAPI/cries/main/cries/pokemon/legacy/${pokemon.id}.ogg`} type="audio/ogg" />
+            Your browser does not support the audio element.
+          </audio>
+        </div>
+        {/* Afficher les sprites */}
+        <div className="mt-4">
+          <h2 className="text-2xl font-bold">Sprites</h2>
+          <div className="grid grid-cols-2 gap-4">
+            {pokemon.sprites.front_default && (
+              <div>
+                <h3 className="capitalize">Front Default</h3>
+                <Image
+                  src={pokemon.sprites.front_default}
+                  alt={`${pokemon.name} front default`}
+                  className="w-full"
+                  width={100}
+                  height={100}
+                />
+              </div>
+            )}
+            {pokemon.sprites.back_default && (
+              <div>
+                <h3 className="capitalize">Back Default</h3>
+                <Image
+                  src={pokemon.sprites.back_default}
+                  alt={`${pokemon.name} back default`}
+                  className="w-full"
+                  width={100}
+                  height={100}
+                />
+              </div>
+            )}
+            {pokemon.sprites.front_shiny && (
+              <div>
+                <h3 className="capitalize">Front Shiny</h3>
+                <Image
+                  src={pokemon.sprites.front_shiny}
+                  alt={`${pokemon.name} front shiny`}
+                  className="w-full"
+                  width={100}
+                  height={100}
+                />
+              </div>
+            )}
+            {pokemon.sprites.back_shiny && (
+              <div>
+                <h3 className="capitalize">Back Shiny</h3>
+                <Image
+                  src={pokemon.sprites.back_shiny}
+                  alt={`${pokemon.name} back shiny`}
+                  className="w-full"
+                  width={100}
+                  height={100}
+                />
+              </div>
+            )}
+          </div>
+        </div>
+      </div>
+    </div>
     </div>
   );
 }
--- src/datas/PokemonList.tsx
+++ src/datas/PokemonList.tsx
@@ -1,18 +1,21 @@
 // app/PokemonList.tsx
 "use client";
-import Image from 'next/image';
 // app/PokemonList.tsx
-import Link from 'next/link';
-import React, { useState, useEffect } from 'react';
+import Link from "next/link";
+import React, { useState, useEffect } from "react";
+import { capitalizeFirstLetter } from "../lib/utils"; // Importez la fonction utilitaire
+import Image from "next/image";
 
 // Constant pour le nombre de Pokémon à charger à chaque fois
 const ITEMS_PER_PAGE = 100;
 
 // Fonction pour récupérer les Pokémon depuis l'API
 async function fetchPokemons(offset: number = 0) {
-  const res = await fetch(`https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${ITEMS_PER_PAGE}`);
+  const res = await fetch(
+    `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${ITEMS_PER_PAGE}`
+  );
   if (!res.ok) {
-    throw new Error('Failed to fetch data');
+    throw new Error("Failed to fetch data");
   }
   return res.json();
 }
@@ -29,7 +32,7 @@ export default function PokemonList() {
         data.results.map(async (result: any) => {
           const res = await fetch(result.url);
           if (!res.ok) {
-            throw new Error('Failed to fetch Pokemon details');
+            throw new Error("Failed to fetch Pokemon details");
           }
           return res.json();
         })
@@ -37,7 +40,7 @@ export default function PokemonList() {
       setPokemons((prevPokemons) => [...prevPokemons, ...newPokemons]);
       setNextOffset(nextOffset + ITEMS_PER_PAGE);
     } catch (error) {
-      console.error('Error loading more pokemons:', error);
+      console.error("Error loading more pokemons:", error);
     }
   };
 
@@ -47,28 +50,40 @@ export default function PokemonList() {
   }, []);
 
   return (
-    <div>
-      <h1>Pokemon List</h1>
-      <ul className="flex flex-wrap justify-center">
+    <div className="container mx-auto">
+      <h1 className=" font-bold text-center my-8 px-6 py-4 text-3xl">Pokemon List</h1>
+      <div className="flex flex-wrap justify-center gap  ">
         {pokemons.map((pokemon: any) => (
-          <li key={pokemon.name} className='hover:scale-110 transition duration-300'>
+          <div
+            key={pokemon.name}
+            className="max-w-sm overflow-hidden shadow-lg  hover:scale-110 transition duration-300 glassmorphism-container"
+          >
             <Link href={`/pokedex/${pokemon.name}`}>
-              <div>
-                <p>{pokemon.name}</p>
-                {pokemon.sprites && (
-                  <Image
-                    src={pokemon.sprites.other['official-artwork'].front_default}
-                    alt={pokemon.name}
-                    width= {100}
-                    height= {100}
-                  />
-                )}
+              {pokemon.sprites && (
+                <Image
+                  src={pokemon.sprites.other["official-artwork"].front_default}
+                  alt={pokemon.name}
+                  width={200}
+                  height={200}
+                />
+              )}
+              <div className="px-6 py-4">
+                <div className="font-bold text-xl mb-2 text-center">
+                  {capitalizeFirstLetter(pokemon.name)}
+                </div>
               </div>
             </Link>
-          </li>
+          </div>
         ))}
-      </ul>
-      <button onClick={loadMorePokemons}>Load More</button>
+      </div>
+      <div className="flex justify-center my-8">
+        <button
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+          onClick={loadMorePokemons}
+        >
+          Load More
+        </button>
+      </div>
     </div>
   );
 }
